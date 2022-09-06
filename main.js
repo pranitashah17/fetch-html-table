8d2fed00bd174f5f49e1c017eb6af53b049e5ef6
pranitashah17 <99394961+pranitashah17@users.noreply.github.com>
2022-09-06 15:57:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,19 +16,3 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     document.getElementById("users").innerHTML = li;
   });
 
-etch("https://jsonplaceholder.typicode.com/posts", {
- method: "POST",
-
- body: JSON.stringify({
-    title: "foo",
-    body: "bar",
-    userId: 1
-  }),
-
- headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
- .then((response) => response.json())
-
- .then((json) => console.log(json));
