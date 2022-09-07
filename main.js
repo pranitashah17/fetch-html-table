fa4e5354d58648e393d9fd2208016e751842b755
pranitashah17 <99394961+pranitashah17@users.noreply.github.com>
2022-09-07 11:42:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then((json) => {
     
-    let li = `<th>UserId</th><th>Id</th><th>Title</th> <th>Completed</th>`;
+    let li = `<th>UserId</th> <th>Id</th> <th>Title</th> <th>Completed</th>`;
 
     json.forEach((user) => {
       li += `<tr>
@@ -16,3 +16,24 @@ fetch("https://jsonplaceholder.typicode.com/todos")
     document.getElementById("users").innerHTML = li;
   });
 
+function myFunction() {
+    // Declare variables
+     
+    let filter = document.getElementById("myInput").value.toUpperCase();
+    let table = document.getElementById('users');
+    let tr = table.getElementsByTagName('tr');
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (let i = 0; i < tr.length; i++) {
+      let td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        let txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  
